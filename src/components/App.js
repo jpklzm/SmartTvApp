3bda78134abc35df84f1244e07a8053a6babae85
Jean Menezes <jeanmenezes.nave@gmail.com>
2020-11-08 17:06:34 -0300

    Organizing project folder structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
-import Header from "./components/header/Header";
-import Footer from "./components/footer/Footer";
-import SeriesList from "./components/seriesList/SeriesList";
+import Header from "./header/Header";
+import Footer from "./footer/Footer";
+import SeriesList from "./seriesList/SeriesList";
 import "./App.css";
 
 function App() {
